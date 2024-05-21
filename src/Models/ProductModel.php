77cee6e7fa7452d3db7ca97efa9cd9962e24bbaa
Connor Davis <connordavis115@gmail.com>
2024-05-21 14:31:15 +0100

    SQL injection fix

--- src/Models/ProductModel.php
+++ src/Models/ProductModel.php
@@ -3,10 +3,10 @@
 class ProductModel {
 public static function getProducts(PDO $db, int $id) : array|null
 {
-    $sql = 'SELECT `price`, `color`, `stock` FROM `products` WHERE `category_id` =' . $id .';';
+    $sql = 'SELECT `price`, `color`, `stock` FROM `products` WHERE `category_id` = :id;';
     $query = $db->prepare($sql);
 //    $query->setFetchMode(PDO::FETCH_CLASS, CategoryEntity::class);
-    $query->execute();
+    $query->execute(['id'=>$id]);
     return $query->fetchAll();
 }
 }
